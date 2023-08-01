afba727b08fc21cc98e5228f2d04cde6d7c866c5
Prince Kumar Mathur <101137515+Prince8797@users.noreply.github.com>
2023-08-02 00:04:26 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,6 +8,13 @@ const router = express.Router();
 
 router.post('/signup', userSignup);
 router.post('/login', userLogin);
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+});
 router.get('/payment', addPaymentGateway);
 
 router.get('/products', getProducts);
