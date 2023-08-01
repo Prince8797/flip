290de19801631aa43c7273437bb8a767d50715e3
Prince Kumar Mathur <101137515+Prince8797@users.noreply.github.com>
2023-08-01 23:44:23 +0530

    Update productActions.js

--- shopyfy/src/redux/actions/productActions.js
+++ shopyfy/src/redux/actions/productActions.js
@@ -6,8 +6,12 @@ const URL = "https://ekartweb.onrender.com";
 
 export const getProducts = () => async (dispatch) => { // (dispatch)=> this is a middleware --> feature of thunk
     try {
-        const { data } = await axios.get(`${URL}/products`);
-        dispatch({ type: GET_PRODUCTS_SUCCESS, 'Access-Control-Allow-Credentials': true, payload: data }); //dispatch will internally call productReducer.js
+        const { data } = await axios.get(`${URL}/products`, {
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Replace '*' with the appropriate origin if needed
+            },
+        });
+        dispatch({ type: GET_PRODUCTS_SUCCESS, payload: data }); //dispatch will internally call productReducer.js
     } catch (error) {
         dispatch({ type: GET_PRODUCTS_FAIL, payload: error.message });
     }
@@ -17,9 +21,13 @@ export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: GET_PRODUCT_DETAILS_REQUEST });
 
-        const { data } = await axios.get(`${URL}/product/${id}`);
+        const { data } = await axios.get(`${URL}/product/${id}`, {
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Replace '*' with the appropriate origin if needed
+            },
+        });
 
-        dispatch({ type: GET_PRODUCT_DETAILS_SUCCESS, 'Access-Control-Allow-Credentials': true, payload: data });
+        dispatch({ type: GET_PRODUCT_DETAILS_SUCCESS, payload: data });
 
     } catch (error) {
         dispatch({ type: GET_PRODUCT_DETAILS_FAIL, payload: error.message });
